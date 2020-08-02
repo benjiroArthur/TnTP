73d8b6f342bbd1896cee5fbb87bf92b122d8b0f2
Benjamin Arthur <40395048+benjiroArthur@users.noreply.github.com>
2020-08-02 10:22:22 +0000

    fixed SweetAlert

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
          $this->call(RoleTableSeeder::class);
-         $this->call(RegionTableSeeder::class);
          $this->call(UsersTableSeeder::class);
     }
 }
