d85a52453d0688996399453bc25cef58aa765f0b
Benjamin Arthur <40395048+benjiroArthur@users.noreply.github.com>
2020-08-04 19:43:17 +0000

    statistics update

--- resources/js/components/Traveller/HotelShowMini.vue
+++ resources/js/components/Traveller/HotelShowMini.vue
@@ -3,7 +3,7 @@
         <img :src="hotel.image"  class="card-img-top" :alt="hotel.name + ' thumbnail'">
         <div class="card mb-3 " >
             <div class="card-body">
-                <h5 class="card-title">{{hotel.name}} Hotel</h5>
+                <h5 class="card-title">{{hotel.name}}</h5>
                 <button class="btn btn-primary btn-block" @click="$emit('hotel-clicked',hotel)">Book a room</button>
             </div>
         </div>
